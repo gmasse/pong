552867a81fed978ca11089f7ff03889f2abb18f3
Germain Masse <g-dev@masse.me>
2018-09-30 17:02:13 +0200

    add test 8.8.8.8

--- pong.test.js
+++ pong.test.js
@@ -14,6 +14,14 @@ test('Ping 127.0.0.1 returns a number', done => {
     });
 });
 
+test('Ping 8.8.8.8 returns a number', done => {
+    pong.ping(['8.8.8.8'], function(response) {
+            expect(response).toBeGreaterThan(0);
+            done();
+    });
+});
+
+
 test('Ping 192.0.2.1 returns Time-Out', done => {
     pong.ping(['192.0.2.1'], function(response) {
             expect(response).toBe(-1);
