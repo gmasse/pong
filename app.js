51c4f2323189cf95d418aa69f84a7f464442959b
Germain Masse <g-dev@masse.me>
2018-09-30 18:15:40 +0200

    debug raw

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var pong = require('./pong')
 
-pong.ping(['8.8.8.8'], function (value) {
-    console.log("return: " + value);
-});
+//pong.ping(['8.8.8.8'], function (value) {
+//    console.log("return: " + value);
+//});
+
+pong.raw(['8.8.8.8']);
